3b1eb022a2de7ce3cca86cbd4e2abeaf500087e7
Janusz Bugajny <janusz.bugajny@cybercom.com>
2016-03-16 16:09:40 +0100

    replaced loadNpmTask with load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,9 +37,7 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+require('load-grunt-tasks')(grunt);
 
   grunt.registerTask('build', [
       'connect',
